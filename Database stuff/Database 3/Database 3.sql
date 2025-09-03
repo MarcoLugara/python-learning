6cdd0a99b9030aec1b280f09861bce5190e7942b
marcolughi <dfvsfvfsdvsdavbz@gmail.com>
2025-09-04 00:25:36 +0200

    Commit files

--- Database stuff/Database 3/Database 3.sql
+++ Database stuff/Database 3/Database 3.sql
@@ -1,1 +1,4 @@
-SELECT Album.title, Album.artist_id, Artist.id, Artist.name FROM Album JOIN Artist ON Album.artist_id=Artist.id
+SELECT User.name, Member.role, Course.title FROM User
+	JOIN Member ON User.id = Member.user_id
+	JOIN Course ON Course.id = Member.course_id
+	ORDER BY Course.title, Member.role DESC, User.name
