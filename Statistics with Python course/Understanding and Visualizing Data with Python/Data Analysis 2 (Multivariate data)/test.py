fb8e31a9152a8dae33b932e326dc348de8536559
marcolughi <dfvsfvfsdvsdavbz@gmail.com>
2025-10-12 19:57:16 +0200

    Commit files

--- Statistics with Python course/Understanding and Visualizing Data with Python/Data Analysis 2 (Multivariate data)/test.py
+++ Statistics with Python course/Understanding and Visualizing Data with Python/Data Analysis 2 (Multivariate data)/test.py
@@ -8,18 +8,6 @@ pd.set_option('display.max_columns', 100)
 
 path = "NHANES.csv"
 df = pd.read_csv(path)
-bp = df['BPXSY2']
-bp_median = bp.median()
-print(bp_median)
 
-bp_mean = bp.mean()
-print(bp_mean)
-
-bp_sd = bp.std()
-print(bp_sd)
-
-bp_max = bp.max()
-print(bp_max)
-
-bp_IQR = bp.quantile(0.75) - bp.quantile(0.25)
-print(bp_IQR)
+unique_values = df["SEQN"].unique()
+print(unique_values)
---
+++ Valter Gamba article/main.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import scipy.stats as stats
+#matplotlib inline
+import matplotlib.pyplot as plt
+
+
+path = "DATASET.csv"
+df = pd.read_csv(path)
